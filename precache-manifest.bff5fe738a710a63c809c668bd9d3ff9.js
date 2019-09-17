88c59d2cdc0ee2a32d4108964ff6163b9072932c
Gildard <gildardmhn93@gmail.com>
2019-09-16 23:41:21 -0300

    Updates

---
+++ precache-manifest.bff5fe738a710a63c809c668bd9d3ff9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7969f060eabd0ddfe0b296f43dc62c98",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9adcfdad56adc1435b14",
+    "url": "/react-monsters-rolodex/static/css/main.3a2a20ab.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/react-monsters-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "9adcfdad56adc1435b14",
+    "url": "/react-monsters-rolodex/static/js/main.fa66412d.chunk.js"
+  },
+  {
+    "revision": "e9ac89f6a45bacaf16fe",
+    "url": "/react-monsters-rolodex/static/js/runtime~main.eb0d3c9f.js"
+  }
+]);
