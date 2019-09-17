a9960a32e4d2b80f0a1ea5e343ed3c0fcbd7e9fe
Gildard <gildardmhn93@gmail.com>
2019-09-16 23:28:40 -0300

    link

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
     }
 
     componentDidMount () {
-        fetch('https://jsonplaceholder.typicode.com/users')
+        fetch('https://jsonplaceholder.typicode.com/')
         //fetching objects from jsonplaceholder
         .then(response => response.json())
         .then(users => this.setState({ monsters: users }))
